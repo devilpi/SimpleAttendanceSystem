06db4afdd03ade4cdbd6e22a8ef7aedb5b6bb4ad
devilpi <517348646@qq.com>
2018-01-06 16:02:29 +0800

    Initial commit

---
+++ src/main/java/dao/DBUtils.java
@@ -0,0 +1,27 @@
+package main.java.dao;
+
+import java.sql.*;
+import java.util.Properties;
+
+import com.ibatis.common.jdbc.ScriptRunner;
+import com.ibatis.common.resources.Resources;
+/**
+ * Created by devilpi on 06/01/2018.
+ */
+public class DBUtils {
+    static DBHelper dbHelper = new DBHelper("attendance");
+    public static ResultSet executeSql(String sql) {
+        ResultSet resultSet = null;
+        try {
+            Statement stmt = dbHelper.conn.createStatement();
+            resultSet = stmt.executeQuery(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public static void executeUpdate(String sql) {
+
+    }
+}
